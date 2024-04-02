9ceb63235f68df81735f489b8adadc52959f534e
Benshan Mei <mbs2021@163.com>
2024-04-03 02:19:05 +0800

    Fixed debug.h.

--- include/debug.h
+++ include/debug.h
@@ -1,6 +1,11 @@
 #ifndef __DEBUG_H__
+#define __DEBUG_H__
 
 #include "vmpl.h"
 
+#ifdef CONFIG_DUNE_BOOT
 void dune_debug_handle_int(struct dune_config *conf);
+#else
+static inline void dune_debug_handle_int(struct dune_config *conf) {}
+#endif
 #endif
