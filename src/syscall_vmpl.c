a0efa01b59af80b119b6915456646ab4d9890498
Benshan Mei <mbs2021@163.com>
2024-04-13 00:27:33 +0800

    Add `vmpl_syscall` wrapper.

---
+++ src/syscall_vmpl.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+struct syscall_args_t {
+    long a0, a1, a2, a3, a4, a5;
+} __attribute__((packed));
+
+long vmpl_syscall(long sys_nr, struct syscall_args_t *args) {
+    syscall(sys_nr, args->a0, args->a1, args->a2, args->a3, args->a4, args->a5);
+}
